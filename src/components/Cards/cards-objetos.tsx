a3cd96ef3016f042a2b9d7443e9301bc5c289876
ericksantos2 <erick.dos.santos.940@icloud.com>
2023-01-26 21:19:15 -0300

    coloquei os arquivos do react dnv

---
+++ src/components/Cards/cards-objetos.tsx
@@ -0,0 +1,26 @@
+const objetoCards: any[] = [
+  {
+    qualidade: {
+      nome: "",
+      id: "",
+      tipo: "",
+    },
+    clube: {
+      nome: "",
+      id: "",
+      tipo: "",
+    },
+    liga: {
+      nome: "",
+      id: "",
+      tipo: "",
+    },
+    nacionalidade: {
+      nome: "",
+      id: "",
+      tipo: "",
+    },
+  },
+];
+
+export default objetoCards;
---
+++ src/types/cadastro-objeto.ts
@@ -0,0 +1,8 @@
+export interface cadastroTipo {
+  cadaInput: cadastroInputsTipo[];
+}
+
+export interface cadastroInputsTipo {
+  nome: string;
+  id: string;
+}
